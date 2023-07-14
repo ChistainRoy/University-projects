00ea4574fbace5ce26d3230abcb9d18465053500
Songpon Chumtrong <redmeowx@gmail.com>
2023-07-14 20:28:44 +0700

    update detail

--- html/detail.php
+++ html/detail.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Order - Detail | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
 
@@ -15,7 +16,9 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
@@ -40,47 +43,53 @@
     <script src="../assets/js/config.js"></script>
 </head>
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Sarabun:wght@200&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Sarabun:wght@200&display=swap');
 
-    .navbar {
-        background-color: #ffffff;
-        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-    }
+.navbar {
+    background-color: #ffffff;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
 
-    a.navbar-brand {
-        color: white;
-    }
+a.navbar-brand {
+    color: white;
+}
 
-    span {
-        font-family: 'Sarabun', sans-serif;
-    }
+span {
+    font-family: 'Sarabun', sans-serif;
+}
 
-    span.uppercase {
+span.uppercase {
 
-        text-transform: uppercase;
-    }
+    text-transform: uppercase;
+}
 
-    h5 {
-        font-family: 'Sarabun', sans-serif;
-    }
-    h3{
-        color: #ffff;
-    }
-    h2{
-        color: #696cff;
-    }
-    body{
-        background: -webkit-linear-gradient(left, #e0e0e0, #696cff);
-    }
-    .card-header{
-        background: #696cff;
-    }
-    .img{
-        margin: auto auto;
-    }
-    .preview{
-        margin: auto auto;
-    }
+h5 {
+    font-family: 'Sarabun', sans-serif;
+}
+
+h3 {
+    color: #ffff;
+}
+
+h2 {
+    color: #696cff;
+}
+
+body {
+    background: -webkit-linear-gradient(left, #e0e0e0, #696cff);
+}
+
+.card-header {
+    background: #696cff;
+}
+
+.img {
+    margin: auto auto;
+}
+
+.preview {
+    margin: auto auto;
+}
 </style>
 
 <body>
@@ -88,21 +97,23 @@
         <div class="container p-5 mt-5">
             <div class="row">
                 <div class="col-xl-6">
-            <div class="card">
-                <h3 class="card-header text-center mb-3">รายละเอียดคำสั่งซื้อ</h3>
-               
-                <?php
+                    <div class="card">
+                        <h3 class="card-header text-center mb-3">รายละเอียดคำสั่งซื้อ</h3>
+
+                        <?php
                  include('connect.php');
                 if(isset($_GET['ids'])){
                         $id = $_GET['ids'];
                         $sql = "SELECT * FROM `order` INNER JOIN oderdetail ON `order`.`order_id` = oderdetail.oder_id
                         INNER JOIN product ON oderdetail.product_id = product.product_id WHERE `order_id` = $id";
                         $result = mysqli_query($conn, $sql);
-                        while($row = mysqli_fetch_assoc($result)) { ?>   
+                        while($row = mysqli_fetch_assoc($result)) { ?>
                         <div class="card-body">
                             <div class="d-flex align-items-start align-items-sm-center gap-4">
-                                <img src="<?php echo $row['product_img'] ?>" alt="user-avatar" class="d-block rounded" height="150" width="120"/>
-                                <p><?php echo $row['product_name'] ?><br>ขนาด <?php echo $row['product_width'] ?> X <?php echo $row['product_length'] ?></p>
+                                <img src="<?php echo $row['product_img'] ?>" alt="user-avatar" class="d-block rounded"
+                                    height="150" width="120" />
+                                <p><?php echo $row['product_name'] ?><br>ขนาด <?php echo $row['product_width'] ?> X
+                                    <?php echo $row['product_length'] ?></p>
                                 <h5 class="mx-5">ราคา</h5>
                                 <h5><?php echo $row['product_price'] ?></h5>
                                 <h5>X</h5>
@@ -113,56 +124,57 @@
                             </div>
                             <hr>
                         </div>
-                       
-            <?php   } 
-            ?>          
-                      
-            <?php
+
+                        <?php   } 
+            ?>
+
+                        <?php
             }
             ?>
-            </div>
-            </div>  <div class="col-xl-4">
-            <div class="card">
-                <h3 class="card-header text-center mb-3">สรุปยอดคำสั่งซื้อ</h3>
-                <div class="card-body p-3">
-                    <p>ที่อยู่จัดส่ง</p>
-                    <h5 class="mx-2"><?php echo $address ?></h5>    
-                    <p>ราคาทั้งหมด</p>
-                    <h5 class="mx-2"><?php echo $total ?></h5>
-                    <p>ราคามัดจำที่ต้องจ่าย (20%)</p>
-                    <h5 class="mx-2"><?php echo $price ?></h5>       
-                    <from>
-                    </from> 
                     </div>
                 </div>
-                <div class="card mt-5">
-                <h3 class="card-header text-center mb-3">ชำระเงิน</h3>
-                <img src="upload/scan.png" alt class="img d-block rounded p-2" height="200" width="200" />
-                <div class="card-body p-3">
-                        <label for="formFile" class="form-label">กดปุ่มอัปโหลดสลิป</label>
-                        <input class="form-control" type="file" id="formFile" name="pic"  />
-                        <br>
-                        <img src="" id="preview" width="200" class="preview">
-                </div>
+                <div class="col-xl-4">
+                    <div class="card">
+                        <h3 class="card-header text-center mb-3">สรุปยอดคำสั่งซื้อ</h3>
+                        <div class="card-body p-3">
+                            <p>ที่อยู่จัดส่ง</p>
+                            <h5 class="mx-2"><?php echo $address ?></h5>
+                            <p>ราคาทั้งหมด</p>
+                            <h5 class="mx-2"><?php echo $total ?></h5>
+                            <p>ราคามัดจำที่ต้องจ่าย (20%)</p>
+                            <h5 class="mx-2"><?php echo $price ?></h5>
+                            <from>
+                            </from>
+                        </div>
+                    </div>
+                    <div class="card mt-5">
+                        <h3 class="card-header text-center mb-3">ชำระเงิน</h3>
+                        <img src="upload/scan.png" alt class="img d-block rounded p-2" height="200" width="200" />
+                        <div class="card-body p-3">
+                            <label for="formFile" class="form-label">กดปุ่มอัปโหลดสลิป</label>
+                            <input class="form-control" type="file" id="formFile" name="pic" />
+                            <br>
+                            <img src="" id="preview" width="200" class="preview">
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     </section>
     <script>
-                            const input = document.getElementById('formFile');
-                            const preview = document.getElementById('preview');
+    const input = document.getElementById('formFile');
+    const preview = document.getElementById('preview');
 
-                            input.addEventListener('change', () => {
-                              const file = input.files[0];
-                              const reader = new FileReader();
+    input.addEventListener('change', () => {
+        const file = input.files[0];
+        const reader = new FileReader();
 
-                              reader.addEventListener('load', () => {
-                                preview.src = reader.result;
-                              });
+        reader.addEventListener('load', () => {
+            preview.src = reader.result;
+        });
 
-                              reader.readAsDataURL(file);
-                            });
-                          </script>
+        reader.readAsDataURL(file);
+    });
+    </script>
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
     <script src="../assets/vendor/libs/jquery/jquery.js"></script>
