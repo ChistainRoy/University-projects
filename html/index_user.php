3bc30ed6c40f741c2d8cb6ff1bfe9ad9e8c780bd
Songpon Chumtrong <redmeowx@gmail.com>
2023-08-30 16:54:43 +0700

    g

--- html/index_user.php
+++ html/index_user.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Home - Page | Buddy-Aluminum</title>
 
@@ -16,9 +15,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
@@ -43,82 +40,82 @@
     <script src="../assets/js/config.js"></script>
 </head>
 <style>
-.navbar {
-    background-color: #ffffff;
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-}
-
-a.navbar-brand {
-    color: white;
-}
-
-.num {
-    font-family: 'Sigmar', cursive;
-    background-color: red;
-    position: absolute;
-    color: white;
-    font-size: 14px;
-    margin-left: 150px;
-    margin-bottom: 50px;
-    margin-top: -37px;
-    width: 30px;
-    text-align: center;
-    padding: 4px;
-    border-radius: 20px;
-}
-
-.carousel-item {
-    width: 100%;
-    height: 700px;
-}
-
-.slide {
-    width: 100%;
-    height: 700px;
-}
-
-.card-img-top {
-    width: 100%;
-    height: 400px;
-}
-
-@media (max-width:767px) {
-    .slide {
-        max-width: 100%;
-        height: 200px;
+    .navbar {
+        background-color: #ffffff;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
     }
-}
 
-@media (max-width:767px) {
-    .carousel-item {
-        max-width: 100%;
-        height: 200px;
+    a.navbar-brand {
+        color: white;
     }
-}
 
-.dropdown .dropdown-menU {
-    display: none:
-}
-
-.dropdown:hover>.dropdown-menu,
-.dropend:hover>.dropdown-menu {
-    display: block;
-    margin-top: .125em;
-    margin-left: .125em;
-}
-
-@media screen and (min-width:769px) {
-    .dropend:hover>.dropdown-menu {
+    .num {
+        font-family: 'Sigmar', cursive;
+        background-color: red;
         position: absolute;
-        top: 0;
-        left: 100%;
+        color: white;
+        font-size: 14px;
+        margin-left: 150px;
+        margin-bottom: 50px;
+        margin-top: -37px;
+        width: 30px;
+        text-align: center;
+        padding: 4px;
+        border-radius: 20px;
     }
-}
+
+    .carousel-item {
+        width: 100%;
+        height: 700px;
+    }
+
+    .slide {
+        width: 100%;
+        height: 700px;
+    }
+
+    .card-img-top {
+        width: 100%;
+        height: 400px;
+    }
+
+    @media (max-width:767px) {
+        .slide {
+            max-width: 100%;
+            height: 200px;
+        }
+    }
+
+    @media (max-width:767px) {
+        .carousel-item {
+            max-width: 100%;
+            height: 200px;
+        }
+    }
+
+    .dropdown .dropdown-menU {
+        display: none;
+    }
+
+    .dropdown:hover>.dropdown-menu,
+    .dropend:hover>.dropdown-menu {
+        display: block;
+        margin-top: .125em;
+        margin-left: .125em;
+    }
+
+    @media screen and (min-width:769px) {
+        .dropend:hover>.dropdown-menu {
+            position: absolute;
+            top: 0;
+            left: 100%;
+        }
+    }
 </style>
 
 <body>
     <?php
-        session_start();
+    session_start();
     if (!isset($_SESSION['username_user'])) {
         header("location: login.php");
     }
@@ -131,9 +128,7 @@ a.navbar-brand {
     <nav class="navbar navbar-expand-lg navbar-light sticky-top">
         <div class="container-fluid">
             <img src="upload/b.png" width="50">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -145,8 +140,7 @@ a.navbar-brand {
                         <a class="nav-link" href="javascript:void(0)">เกี่ยวกับร้าน</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link" href="javascript:void(0)" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link" href="javascript:void(0)" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             สินค้า
                         </a>
                         <ul class="dropdown-menu text-center" aria-labelledby="navbarDropdown">
@@ -160,8 +154,7 @@ a.navbar-brand {
 
                             </li>
                             <li class="nav-item dropend">
-                                <a class="nav-link" href="javascript:void(0)" id="navbarDropdown" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link" href="javascript:void(0)" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     สินค้า
                                 </a>
                                 <ul class="dropdown-menu text-center" aria-labelledby="navbarDropdown">
@@ -187,14 +180,12 @@ a.navbar-brand {
                     <button class="btn btn-outline-primary" type="submit">Search</button>
                 </form>
                 <div class="btn-group mx-2">
-                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                         <?php echo $_SESSION['username_user'] ?>
                     </button>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="javascript:void(0);">แก้ไขข้อมูลส่วนตัว</a></li>
-                        <li><a class="dropdown-item" href="myorder.php">ออเดอร์ของฉัน&nbsp;&nbsp;<span
-                                    class="badge rounded-pill badge-center h-px-20 w-px-20 bg-label-danger">1</span></a>
+                        <li><a class="dropdown-item" href="myorder.php">ออเดอร์ของฉัน&nbsp;&nbsp;<span class="badge rounded-pill badge-center h-px-20 w-px-20 bg-label-danger">1</span></a>
                         </li>
                         <li>
                             <hr class="dropdown-divider" />
